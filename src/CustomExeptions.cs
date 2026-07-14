851583b059f75cb3b83eab61c5241bf144b3f3c6
Mordechai Bar-tal <mordechay0tzvi@gmail.com>
2026-07-14 11:53:33 +0300

    custom exeptions

---
+++ src/CustomExeptions.cs
@@ -0,0 +1,13 @@
+namespace drones
+{
+    class InvalidId :Exception;
+    class InvalidSerialNumber;
+    class InvalidModel;
+    class InvalidCategory;
+    class InvalidBaseLocation;
+    class InvalidFlightHours;
+    class InvalidBatteryHealth;
+    class InvalidMaxRangeKm;
+    class InvalidMissionsCompleted;
+    class InvalidStatus;
+}
