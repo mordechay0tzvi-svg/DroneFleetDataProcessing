2b454241ece093b5b0117ab6161a9e2073830940
avraham <aviv4134@gmail.com>
2026-07-14 16:53:49 +0300

    fun: process json to object

--- DroneFleetDataProcessing/src/DroneValidator.cs
+++ DroneFleetDataProcessing/src/DroneValidator.cs
@@ -15,16 +15,16 @@ namespace drones
         public bool Validate(Drone drone)
         {
             bool valid = true;
-            valid &= idValidate.Validate(drone.Id);
-            valid &= serialNumberValidator.Validate(drone.SerialNumber);
-            valid &= modelValidator.Validate(drone.Model);
-            valid &= categoryValidator.Validate(drone.Category);
-            valid &= baseLocationValidator.Validate(drone.BaseLocation);
-            valid &= flightHoursValidator.Validate(drone.FlightHours);
-            valid &= batteryHealthValidator.Validate(drone.BatteryHealth);
-            valid &= maxRangeValidator.Validate(drone.MaxRangeKm);
-            valid &= missionsCompletedValidator.Validate(drone.MissionsCompleted);
-            valid &= statusValidator.Validate(drone.Status);
+            valid &= idValidate.Validate(drone.id);
+            valid &= serialNumberValidator.Validate(drone.serialNumber);
+            valid &= modelValidator.Validate(drone.model);
+            valid &= categoryValidator.Validate(drone.category);
+            valid &= baseLocationValidator.Validate(drone.base_location);
+            valid &= flightHoursValidator.Validate(drone.flightHours);
+            valid &= batteryHealthValidator.Validate(drone.batteryHealth);
+            valid &= maxRangeValidator.Validate(drone.maxRangeKm);
+            valid &= missionsCompletedValidator.Validate(drone.missionsCompleted);
+            valid &= statusValidator.Validate(drone.status);
             return valid;
         }
     }
---
+++ DroneFleetDataProcessing/src/ProcessData/Process.cs
@@ -0,0 +1,23 @@
+﻿using drones;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace DroneFleetDataProcessing.src.ProcessData
+{
+    class Process
+    {
+        static List<Drone> InitialProcess()
+        {
+            string folderPath = Path.Combine("input","raw", "drones_raw.json");
+            string loadJson = File.ReadAllText(folderPath);
+            //Console.WriteLine(string.Join(", ",loadJson));
+
+            var options = new JsonSerializerOptions(); 
+            
+            List<Drone> drones = JsonSerializer.Deserialize<List<Drone>>(loadJson) ?? new();
+            return drones;
+        }
+    }
+}
