dfa1999fef88493708aac33e6621f82624aa6481
Mordechai Bar-tal <mordechay0tzvi@gmail.com>
2026-07-14 15:38:59 +0300

    json files filled

--- DroneFleetDataProcessing/src/CustomExeptions.cs
+++ DroneFleetDataProcessing/src/CustomExeptions.cs
@@ -1,13 +1,13 @@
 namespace drones
 {
     class InvalidId : Exception;
-    class InvalidSerialNumber;
-    class InvalidModel;
-    class InvalidCategory;
-    class InvalidBaseLocation;
-    class InvalidFlightHours;
-    class InvalidBatteryHealth;
-    class InvalidMaxRangeKm;
-    class InvalidMissionsCompleted;
-    class InvalidStatus;
+    class InvalidSerialNumber : Exception;
+    class InvalidModel : Exception
+    class InvalidCategory : Exception;
+    class InvalidBaseLocation : Exception;
+    class InvalidFlightHours : Exception;
+    class InvalidBatteryHealth : Exception;
+    class InvalidMaxRangeKm : Exception;
+    class InvalidMissionsCompleted : Exception;
+    class InvalidStatus : Exception;
 }
